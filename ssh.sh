e17cdc31d6df1aed6fad35448166794184ae344e
X-Dean <deanxaka@gmail.com>
2022-04-17 23:56:46 +0200

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+if ! [ $(id -u) = 0 ]; then
+   echo "Ky script duhet te ekzekutohet si user root, ju lutem perdorni "sudo" " &>/dev/null
+   exit 1
+fi
+
 sed -i -e 's|SHELL=/bin/sh|SHELL=/bin/bash|' /etc/default/useradd &>/dev/null
 sed -i -e 's|# HOME=/home|HOME=/home|' /etc/default/useradd &>/dev/null
 sed -i -e 's|# SKEL=/etc/skel|SKEL=/etc/skel|' /etc/default/useradd &>/dev/null
 sed -i -e 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config &>/dev/null
 sed -i -e 's|ChallengeResponseAuthentication no|ChallengeResponseAuthentication yes|' /etc/ssh/sshd_config &>/dev/null
 systemctl restart sshd.service
-
-if ! [ $(id -u) = 0 ]; then
-   echo "Ky script duhet te ekzekutohet si user root, ju lutem perdorni "sudo" " &>/dev/null
-   exit 1
-fi
 
 echo "Ju lutem fusni emrin e userit te cilin doni te shtoni ne sistem :"
 
@@ -21,5 +21,5 @@ if [ $? -ne 0 ]; then
       usermod -aG sudo $user
       passwd $user
 else
-        echo "Useri me kete emer ekziston, ju lutem provo perseri:"
+        echo "Useri me kete emer ekziston, ju lutem provo perseri"
 fi
